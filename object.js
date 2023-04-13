68c1fb951265f8d56ea861ce8bf8c084d3d336eb
knymizx <128163268+knymizx@users.noreply.github.com>
2023-04-13 19:45:10 -0400

    Add files via upload

---
+++ object.js
@@ -0,0 +1,14 @@
+const myObject = {
+    make: "BMW",
+    model: "530",
+    doors: 4,
+    color: "grape"
+}
+
+const myObject = {
+    gender: "male",
+    height: "6'3",
+    weight: "200",
+    build: "athletic",
+
+}
